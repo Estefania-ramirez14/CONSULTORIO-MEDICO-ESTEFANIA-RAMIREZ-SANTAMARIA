4e235dd796a8c402056d302bf93044098dd002b2
eramirezs <estefania.ramirezhmo@gmail.com>
2026-06-05 16:15:07 -0700

    Agregar gestor de archivos CSV

--- src/GestorArchivos.java
+++ src/GestorArchivos.java
@@ -1,2 +1,71 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class GestorArchivos {
+
+    private final String CARPETA_DB = "db/";
+
+    public void guardarDoctores(ArrayList<Doctor> doctores) {
+
+        File archivo = new File(CARPETA_DB + "doctores.csv");
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(archivo))) {
+
+            for (Doctor doctor : doctores) {
+
+                writer.println(
+                        doctor.getIdDoctor() + "," +
+                                doctor.getNombreCompleto() + "," +
+                                doctor.getEspecialidad()
+                );
+            }
+
+            System.out.println("Doctores guardados correctamente.");
+
+        } catch (IOException e) {
+
+            System.out.println("Error al guardar doctores.");
+        }
+    }
+
+    public void guardarPacientes(ArrayList<Paciente> pacientes) {
+
+        File archivo = new File(CARPETA_DB + "pacientes.csv");
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(archivo))) {
+
+            for (Paciente paciente : pacientes) {
+
+                writer.println(
+                        paciente.getIdPaciente() + "," +
+                                paciente.getNombreCompleto()
+                );
+            }
+
+            System.out.println("Pacientes guardados correctamente.");
+
+        } catch (IOException e) {
+
+            System.out.println("Error al guardar pacientes.");
+        }
+    }
+
+    public void guardarCitas(ArrayList<Cita> citas) {
+
+        File archivo = new File(CARPETA_DB + "citas.csv");
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(archivo))) {
+
+            for (Cita cita : citas) {
+
+                writer.println(cita.getDatos());
+            }
+
+            System.out.println("Citas guardadas correctamente.");
+
+        } catch (IOException e) {
+
+            System.out.println("Error al guardar citas.");
+        }
+    }
 }
