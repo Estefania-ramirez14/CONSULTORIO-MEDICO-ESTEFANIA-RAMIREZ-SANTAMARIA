786d424f0e5418affbb6eaa1ede4af90d1e8bbc5
eramirezs <estefania.ramirezhmo@gmail.com>
2026-06-05 16:16:55 -0700

    Agregar manejo de excepciones

--- src/ManejadorExcepciones.java
+++ src/ManejadorExcepciones.java
@@ -1,2 +1,25 @@
 public class ManejadorExcepciones {
+
+    private String ultimoError;
+
+    public void mostrarError(String mensaje) {
+
+        ultimoError = mensaje;
+
+        System.out.println("ERROR: " + mensaje);
+    }
+
+    public void registrarError(Exception e) {
+
+        ultimoError = e.getMessage();
+
+        System.out.println(
+                "Se produjo una excepción: " +
+                        e.getMessage()
+        );
+    }
+
+    public String getUltimoError() {
+        return ultimoError;
+    }
 }
