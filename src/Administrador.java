6efbb3bd1aa5541c80c18c9cfda9321c9456bb2f
eramirezs <estefania.ramirezhmo@gmail.com>
2026-06-05 16:04:32 -0700

    Agregar clase Administrador

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,2 +1,30 @@
 public class Administrador {
+
+    private String idAdmin;
+    private String usuario;
+    private String password;
+
+    public Administrador(String idAdmin, String usuario, String password) {
+        this.idAdmin = idAdmin;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public boolean iniciarSesion(String usuario, String password) {
+
+        return this.usuario.equals(usuario)
+                && this.password.equals(password);
+    }
+
+    public void cerrarSesion() {
+        System.out.println("Sesión cerrada.");
+    }
+
+    public String getIdAdmin() {
+        return idAdmin;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
 }
