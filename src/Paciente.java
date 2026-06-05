e5f78240d58d69bbac2d02303a2464edba2ab8ad
eramirezs <estefania.ramirezhmo@gmail.com>
2026-06-05 15:43:51 -0700

    Agregar clase Paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,2 +1,23 @@
 public class Paciente {
+
+    private String idPaciente;
+    private String nombreCompleto;
+
+    public Paciente(String idPaciente, String nombreCompleto) {
+        this.idPaciente = idPaciente;
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getIdPaciente() {
+        return idPaciente;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getDatos() {
+        return "ID: " + idPaciente +
+                " | Nombre: " + nombreCompleto;
+    }
 }
