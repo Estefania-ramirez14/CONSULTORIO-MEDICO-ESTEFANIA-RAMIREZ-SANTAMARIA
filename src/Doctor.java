1c35ca36f00b5c5a206b4a49c06717f1d57f2deb
eramirezs <estefania.ramirezhmo@gmail.com>
2026-06-05 15:37:39 -0700

    Agregar clase Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,2 +1,30 @@
 public class Doctor {
+
+    private String idDoctor;
+    private String nombreCompleto;
+    private String especialidad;
+
+    public Doctor(String idDoctor, String nombreCompleto, String especialidad) {
+        this.idDoctor = idDoctor;
+        this.nombreCompleto = nombreCompleto;
+        this.especialidad = especialidad;
+    }
+
+    public String getIdDoctor() {
+        return idDoctor;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public String getDatos() {
+        return "ID: " + idDoctor +
+                " | Nombre: " + nombreCompleto +
+                " | Especialidad: " + especialidad;
+    }
 }
